d21356dab50110ce331537b778530ebcbcf4ba73
jmg292 <jeff@gnzlabs.com>
2023-10-01 12:06:56 -0500

    Outline tests

---
+++ handshake_test.go
@@ -0,0 +1,19 @@
+package handshake_test
+
+import "testing"
+
+func TestNewChallenge(t *testing.T) {
+
+}
+
+func TestAuthenticateChallenge(t *testing.T) {
+
+}
+
+func TestNewChallengeResponse(t *testing.T) {
+
+}
+
+func TestAuthenticateResponse(t *testing.T) {
+
+}
